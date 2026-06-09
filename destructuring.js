34a5702bf829f72beceffec4c615e0ce81fac889
Pranjal Kumar <pranjalkumar02062004@gmail.com>
2026-06-09 23:21:35 +0530

    upto spread

--- destructuring.js
+++ destructuring.js
@@ -1,11 +1,14 @@
-const user = { name: "Pranjal", age: 21 };
+let user = { name: "Pranjal", age: 21 };
 
 console.log(user);
 
 console.log(user.name,user.age);
 
-const {name, age} = user;
+let {name, age} = user;
+name="Kumar";
+age="22";
 console.log(name,age);
+console.log(user.name,user.age);
 
 const arr = [10, 20];
 const [first, second] = arr;
---
+++ spread.js
@@ -0,0 +1,14 @@
+let arr1=[1,2];
+let arr2=[...arr1,3,4];
+arr1=[5,6];
+console.log(arr2);
+console.log(arr1);
+
+let user={name:"Pranjal"};
+let user1={...user,age:21};
+
+user={address:"Bahuara"};
+
+console.log(user1);
+
+console.log(user);
