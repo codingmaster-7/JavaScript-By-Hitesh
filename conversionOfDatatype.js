31060f0ce7c09019a348687405181110fe62c11a
Pranjal Kumar <pranjalkumar02062004@gmail.com>
2026-06-09 11:44:23 +0530

    conversionOfDatatype

---
+++ conversionOfDatatype.js
@@ -0,0 +1,21 @@
+let num1=33;
+let numString=String(num1);
+let numBoolean=Boolean(num1);
+
+console.log(typeof(num1));
+console.log(typeof(numString));
+console.log(typeof(numBoolean));
+console.log(num1);
+console.log(numString);
+console.log(numBoolean);
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+let nullBoolean=Boolean(null);
+let undefinedBoolean=Boolean(undefined);
+
+console.log(typeof(nullBoolean));
+console.log(typeof(undefinedBoolean));
+console.log(nullBoolean);
+console.log(undefinedBoolean);
--- javaScript.js
+++ javaScript.js
@@ -1,12 +1,3 @@
 //Hare Krishna Hare Ram
-
-let counter=function(n){
-      return function(){
-            return n++;
-      }
-}
-for(i=0;i<10;i++){
-    let x=counter(10);
-     console.log(x);
-}
 console.log("Pranjal");
+console.log(typeof("Pranjal"));
